43782144aac79bc6286d6fd928ad5fe63ef30d65
Tommie <devtommie@gmail.com>
2022-06-08 13:55:25 -0400

    Basic Registration Form
    Created the form with 3 text fields & submit button.
    Added a success message once the submit button is clicked.
    
    Next: Add validation to the fields before succesfully submitting the
    form otherwise supply an error message.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  })
+  const [success, setSuccess] = useState(false)
+
+  const HandleFirstName = (firstNameValue) => {
+    setValues({...values, firstName: firstNameValue})
+  }
+
+  const HandleLastName = (lastNameValue) => {
+    setValues({...values, lastName: lastNameValue})
+  }
+
+  const HandleEmail = (emailValue) => {
+    setValues({...values, email: emailValue})
+  }
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    setSuccess(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form className='formContainer' onSubmit={(e) => HandleSubmit(e)}>
+        {success ? <div className='successfulReg'>Success!</div> : null }
+        <h2 className='headerText'>Basic Registration Form</h2>
+        <input 
+          onChange={(e) => HandleFirstName(e.target.value)} 
+          className="textFields" 
+          value={values.firstName} 
+          type="text" 
+          placeholder="First Name" 
+          name='firstName'
+        ></input>
+        <input
+          onChange={(e) => HandleLastName(e.target.value)} 
+          className="textFields" 
+          value={values.lastName} 
+          type="text" 
+          placeholder="Last Name" 
+          name='lastName'
+        ></input>
+        <input
+          onChange={(e) => HandleEmail(e.target.value)} 
+          className="textFields" 
+          value={values.email} 
+          type="text" 
+          placeholder="Email" 
+          name='email'
+        ></input>
+        <input 
+          className="buttonFields" 
+          type="submit" 
+          value="Register" 
+          name='submit'
+        ></input>
+      </form>
     </div>
   );
 }
